ab953f834f2b45c20c96e58cfc1d83c9c052268e
Elona Kajtazi <MY_NAME@example.com>
2022-10-20 18:13:26 +0200

    ....

--- src/components/ChangeProfilePictureForm.tsx
+++ src/components/ChangeProfilePictureForm.tsx
@@ -1,27 +1,46 @@
-export function ChangeProfilePictureForm({currentUser, setCurrentUser,setChangePic }) {
+import { UserType } from "../types";
+
+type Props = {
+  currentUser: UserType | null;
+  setCurrentUser: React.Dispatch<React.SetStateAction<UserType | null>>;
+  setChangePic: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export function ChangeProfilePictureForm({
+  currentUser,
+  setCurrentUser,
+  setChangePic,
+}: Props) {
+  if (!currentUser) return <h1>Loading</h1>;
   return (
     <div className="sign-in-modal__wrapper ">
       <div className="sign-in-modal__container">
-        <form onSubmit={(e) => {
-            e.preventDefault()
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
             fetch(`http://localhost:4443/users/${currentUser.id}`, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json"
-                }, 
-                body: JSON.stringify({
-                    avatar: e.target.avatar.value
-                })
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                avatar: e.target.avatar.value,
+              }),
             })
-            .then(rsp => rsp.json())
-            .then(data => setCurrentUser(data))
+              .then((rsp) => rsp.json())
+              .then((data) => setCurrentUser(data));
 
-            setChangePic(false)
-
-        }}>
-            <h2 className="title">Change profile picture</h2>
-            <input type="text" name="avatar" className="text-input" placeholder="image url"/>
-            <button className="sign-in-btn">Change</button>
+            setChangePic(false);
+          }}
+        >
+          <h2 className="title">Change profile picture</h2>
+          <input
+            type="text"
+            name="avatar"
+            className="text-input"
+            placeholder="image url"
+          />
+          <button className="sign-in-btn">Change</button>
         </form>
       </div>
     </div>
